edb9f2e9c91ba6cb6da721f9e0262ed297559a04
BlackHattedMiner <73023108+BlackHattedMiner@users.noreply.github.com>
2020-11-26 20:24:11 +0530

    Add files via upload

---
+++ green.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import webbrowser
+from PIL import Image
+st.title("नमस्कारः!!")
+st.header("सिक्किम कि फ़्लोरा और फ़ौना प्रदर्षनावली में आपका हार्दिक स्वागत है!!")
+st.markdown("""
+<style>
+body {
+   background-color: #FFFF99;
+   color: #00000;
+}
+</style>
+    """, unsafe_allow_html=True)
+
+q = st.selectbox(
+    'तो आप किसके बारे में जानना चाहते हैं??',
+    ('तिब्बेतस्य लोमशः', 'कक्ताकी', 'पाटलं', 'कोविदारः', 'कृष्णः भल्लूकः', 'हिम्पातस्य तरक्षुः', 'अपुष्पोभ्दिदः')
+)
+if q == "कृष्णः भल्लूकः":
+    st.error("अस्मिन् चित्रे कृष्णः भल्लूकः अस्ति ; जातिः --> उर्सुस थिबेतनुस")
+    img = Image.open("bear.jpg")
+    st.image(img,width=700)
+if q == "तिब्बेतस्य लोमशः":
+    st.error("अस्मिन् चित्रे तिब्बेतस्य लोमशः अस्ति ; जातिः --> वी.  फेरीलाटा")
+    imga = Image.open("fox.jpg")
+    st.image(imga,width=700)
+if q == "कक्ताकी":
+    st.error("अस्मिन् चित्रे कक्ताकी अस्ति ; जातिः --> नागफणि")
+    imgb = Image.open("cactus.jpg")
+    st.image(imgb,width=700)
+if q == "पाटलं":
+    st.error("अस्मिन् चित्रे एकम् पाटलं अस्ति ; जातिः --> रोसा केनिना")
+    imgc = Image.open("rose.jpg")
+    st.image(imgc,width=700)
+if q == "कोविदारः":
+    st.error("अस्मिन् चित्रे कोविदारः अस्ति ; जातिः --> और्चिदाकाइ")
+    imgd = Image.open("orchid.jpg")
+    st.image(imgd,width=700)
+if q == "हिम्पातस्य तरक्षुः":
+    st.error("अस्मिन् चित्रे हिम्पातस्य तरक्षुः अस्ति ; जातिः --> पी. उनिसिा")
+    imge = Image.open("snow.jpg")
+    st.image(imge,width=700)
+if q == "अपुष्पोभ्दिदः":
+    st.error("अस्मिन् चित्रे अपुष्पोभ्दिदः अस्ति ; जातिः --> पोल्पोदिके")
+    img = Image.open("fern.jpg")
+    st.image(img,width=700)
+st.sidebar.write("हमारे संसाधन")
+
+
+res = 'https://drive.google.com/drive/folders/1jYTR_9eKQVeY5rVuwOXupvxf2hOKySQZ?usp=sharing'
+
+if st.sidebar.button('इस पर क्लिक कीजिये.'):
+    webbrowser.open_new_tab(res)
